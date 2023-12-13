abc6a8a568bc9492bc2fad4fd18c439900ee9030
Balaji <balajitheblankde@gmail.com>
2023-12-13 23:27:29 +0530

    feat : firebase data base change from viplink to slinkdatabase

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getDatabase, } from "firebase/database"
-import {getAuth} from "firebase/auth"
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCnNxilowpLO1xCIfTf5BDh1dWX86Y9y5s",
-  authDomain: "viplinkmain.firebaseapp.com",
-  databaseURL: "https://viplinkmain-default-rtdb.firebaseio.com",
-  projectId: "viplinkmain",
-  storageBucket: "viplinkmain.appspot.com",
-  messagingSenderId: "496435445417",
-  appId: "1:496435445417:web:0f05a06be279450a6cb3b8",
-  measurementId: "G-45E9EHGZEJ"
+     apiKey: "AIzaSyCXtMBYIMuL0HRfskvx7udixPReD1qG_ng",
+     authDomain: "slinkdatabase-4b4ce.firebaseapp.com",
+     databaseURL: "https://slinkdatabase-4b4ce-default-rtdb.firebaseio.com",
+     projectId: "slinkdatabase-4b4ce",
+     storageBucket: "slinkdatabase-4b4ce.appspot.com",
+     messagingSenderId: "905246437476",
+     appId: "1:905246437476:web:866cd663e7c30aa87c6f71",
+     measurementId: "G-L49FLBS1JT",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app)
-export const auth = getAuth(app)
-
-
+export const db = getDatabase(app);
+export const auth = getAuth(app);
